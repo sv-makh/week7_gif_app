49701c4c7ce6dbbc06e48769bf8030bef74e8743
sv-makh <mithriam@mail.ru>
2021-11-07 17:34:20 +0300

    added some comments

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -2,4 +2,5 @@ const giphyApiKey = "zIMgtApjVjS7hHlFPR1zJQt0ovogdsMY";
 
 const giphyUrl = "https://api.giphy.com/v1/gifs/search";
 
+//For beta api keys at giphy.com max limit for this number is 50
 const numberOfGifs = 8;
